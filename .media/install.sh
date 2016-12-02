c5da49ef7bdfdaef9f5a993c88dd5d3b5f683a29
Robert Van Voorhees <rcvanvo@gmail.com>
2016-12-02 15:12:23 -0500

    Adds in configuration copying.

---
+++ .media/install.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh
+
+tar -xvf configuration.tar.gz
+cp configuration/* ~
+sudo dnf install -y nginx
+sudo cp couchpotatoserver.service mylar.service sabnzbd.service sickrage.service  /etc/systemd/system/
+sudo cp media.conf /etc/nginx/conf.d/
