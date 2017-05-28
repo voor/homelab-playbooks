d0187aff3fbbf913d62a8759b571c60435fcc2f4
Robert Van Voorhees <rcvanvo@gmail.com>
2017-05-28 07:41:37 -0400

    Adding in workstation stuff, too.

--- install-atom.sh
+++ install-atom.sh
@@ -2,27 +2,33 @@
 
 sudo dnf install -y fedora-repos-rawhide nodejs jq
 
-sudo dnf install -y `curl -s https://api.github.com/repos/atom/atom/releases/latest | jq --raw-output -c '.assets[] | select(.name | contains("atom.x86_64.rpm")) | .browser_download_url'`
-
 # A bunch of packages I feel help enhance atom well.
 apm install \
   asciidoc-preview \
   atom-beautify \
-  atom-material-syntax \
-  atom-material-syntax-light \
-  atom-material-ui \
-  atom-typescript \
+  autocomplete-js-import \
+  busy-signal \
   color-picker \
   docblockr \
+  fonts \
+  intentions \
   jumpy \
   language-asciidoc \
+  language-babel \
   language-docker \
   language-groovy \
+  language-kickstart \
+  language-protobuf \
+  language-rpm-spec \
   linter \
-  linter-tslint \
+  linter-eslint \
+  linter-ui-default \
   pigments \
   project-manager \
+  react-es6-snippets \
+  react-redux-snippets \
   sort-lines \
   split-diff
+
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
