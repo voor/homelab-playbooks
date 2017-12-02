ef5bdda98e3ab636a006e647dfb42e515aee9813
Robert Van Voorhees <rcvanvo@gmail.com>
2017-12-01 19:56:19 -0500

    Increase file notifier limits.  Other changes for gnome extensions.

--- install-paper.sh
+++ install-paper.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-sudo dnf install gnome-tweak-tool paper-icon-theme paper-gtk-theme
-
 # Popular Gnome Extensions that improve the environment
 mkdir -p ~/.local/share/gnome-shell/extensions/
 git clone https://github.com/voor/audio-output-switcher.git ~/.local/share/gnome-shell/extensions/audio-output-switcher@anduchs && \
   gnome-shell-extension-tool -e audio-output-switcher.git
 git clone https://github.com/voor/audio-input-switcher.git ~/.local/share/gnome-shell/extensions/audio-input-switcher@anduchs && \
   gnome-shell-extension-tool -e audio-input-switcher.git
-git clone https://github.com/v-dimitrov/gnome-shell-extension-stealmyfocus ~/.local/share/gnome-shell/extensions/focus-my-window@varianto25.com && \
+git clone git@github.com:tak0kada/gnome-shell-extension-stealmyfocus.git ~/.local/share/gnome-shell/extensions/steal-my-focus@kagesenshi.org && \
   gnome-shell-extension-tool -e gnome-shell-extension-stealmyfocus
 git clone https://github.com/spin83/multi-monitors-add-on.git /tmp/multi-monitors-add-on && \
   cp -r /tmp/multi-monitors-add-on/multi-monitors-add-on@spin83 ~/.local/share/gnome-shell/extensions && \
@@ -20,13 +18,3 @@ git clone https://github.com/deadalnix/pixel-saver.git /tmp/pixel-saver && \
   cp -r /tmp/pixel-saver/pixel-saver@deadalnix.me ~/.local/share/gnome-shell/extensions && \
   rm -rf /tmp/pixel-saver && \
   gnome-shell-extension-tool -e pixel-saver@deadalnix.me
-
-gsettings set org.gnome.desktop.interface gtk-theme "Paper" && \
-  gsettings set org.gnome.desktop.interface icon-theme "Paper" && \
-  gsettings set org.gnome.desktop.interface cursor-theme "Paper" && \
-  gsettings set org.gnome.desktop.wm.preferences theme "Paper"
-
-mkdir -p ~/.local/share/applications/
-mkdir -p ~/.icons/
-cp -r ./.local/share/applications/ ~/.local/share/
-cp -r ./.icons/ ~
