95141876ce63bc1223e374dd30879e06aac52b6e
Robert Van Voorhees <rcvanvo@gmail.com>
2023-05-11 11:46:00 -0400

    Updating packages.

--- all-cli.sh
+++ all-cli.sh
@@ -10,7 +10,7 @@ sudo dnf -y install \
   $(curl -sL https://api.github.com/repos/vmware-tanzu/octant/releases/latest | jq -r '.assets[].browser_download_url' | grep -e rpm | grep -e 64bit) \
   https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm \
   $(curl -sL https://api.github.com/repos/mozilla/sops/releases/latest | jq -r '.assets[].browser_download_url' | grep -e x86_64 | grep -e rpm) \
-  $(curl -sL https://api.github.com/repos/aquasecurity/trivy/releases/latest | jq -r '.assets[].browser_download_url' | grep -e rpm | grep 64bit) \
+  $(curl -sL https://api.github.com/repos/aquasecurity/trivy/releases/latest | jq -r '.assets[].browser_download_url' | grep -e rpm | grep 64bit | grep -v sig | grep -v pem) \
   $(curl -sL https://api.github.com/repos/tektoncd/cli/releases/latest | jq -r '.assets[].browser_download_url' | grep -e rpm | grep 64bit) \
   $(curl -sL https://api.github.com/repos/anchore/grype/releases/latest | jq -r '.assets[].browser_download_url' | grep -e rpm | grep amd64) \
   https://zoom.us/client/latest/zoom_x86_64.rpm
