46e3be2eb39497b60374fa7a87373a910a5c1bd5
Robert Van Voorhees <rcvanvo@gmail.com>
2025-04-10 11:02:17 -0400

    Updating for arch

---
+++ roles/rogue/templates/sabnzbd-chown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown ruser:ruser -R /var/lib/sabnzbd/
+
+echo "Fixed folder permissions for ruser"
---
+++ roles/rogue/templates/sickchill-chown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown ruser:ruser -R /opt/sickchill/*
+
+echo "Fixed folder permissions for ruser"
